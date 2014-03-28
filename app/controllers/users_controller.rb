856e19779f46ad1252473f82f4f6fec210297133
medbekkouch <bekkouchmohamed@gmail.com>
2014-03-28 15:51:57 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
+		#@user.update_attributes(:email => "example@railstutorial.org", :password => "foobar", :password_confirmation => "foobar")
+		@titre = @user.nom
+		#gravatar_image_tag @user.email
 	end
 	def new
 		@titre = "IInscription"
-		#User.create!(:nom => "Michael Hartl", :email => "mhartl@example.com")
+		#User.create!(:nom => "Utilisateur exemple", :email => "user@example.com", :password => "foobar", :password_confirmation => "foobar")
+		user = User.find_by_email("mhartl@example.com")
+		@vv = user.has_password?("foobar")
 	end
 
 end
